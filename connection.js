c2635f2157bd1ac79fa2f91aead45c2a0349e91b
Fikri ihsan <mfikriihsan03@gmail.com>
2022-02-24 12:19:43 +0700

    create a connection to heroku postgres

--- connection.js
+++ connection.js
@@ -1,14 +1,21 @@
 const { Sequelize } = require("sequelize");
 
 var sequelize = new Sequelize(
-  "df6euqkdmg8293",
-  "fvjmmmhqotmwpw",
-  "56681942048be2de11eab837fa618c7480b504fe47bff11679bdc9e232f91b23",
+  "postgres://fvjmmmhqotmwpw:56681942048be2de11eab837fa618c7480b504fe47bff11679bdc9e232f91b23@ec2-54-157-160-218.compute-1.amazonaws.com:5432/df6euqkdmg8293",
   {
     dialect: "postgres",
-    host: "ec2-54-157-160-218.compute-1.amazonaws.com",
-    port: "5432",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
+
+// var sequelize = new Sequelize("cms", "root", "fimarasa", {
+//   dialect: "postgres",
+// });
 
 module.exports = { db: sequelize };
